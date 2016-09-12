059dcf4913c5cb5666134853d836338518884c59
胡银辉 <huyinhui@huyinhuideMacBook-Pro.local>
2016-09-13 00:46:52 +0800

    完成第一题

---
+++ euler/src/main/java/com/lean/project/euler/euler0001/Main.java
@@ -0,0 +1,23 @@
+package com.lean.project.euler.euler0001;
+
+public class Main {
+
+	public static void main(String[] args) {
+		long sum = 0;
+		for (int i = 0; i < 1000; i++) {
+
+			if (times3_5(i)) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+	}
+
+	private static boolean times3_5(int i) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
