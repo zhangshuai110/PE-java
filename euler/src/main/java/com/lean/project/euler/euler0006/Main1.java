95a1f44274d6b32d4b2ee9d9a2401465a04f16a1
胡银辉 <huyinhui@huyinhuideMacBook-Pro.local>
2016-09-15 11:14:26 +0800

    增加平方和的计算方法

---
+++ euler/src/main/java/com/lean/project/euler/euler0006/Main1.java
@@ -0,0 +1,22 @@
+package com.lean.project.euler.euler0006;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * 计算1到num的平方和
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public static long squareSum(long num) {
+		long r = 0;
+		for (int i = 1; i <= num; i++) {
+			r += i * i;
+		}
+		return r;
+	}
+}
---
+++ euler/src/test/java/com/lean/project/euler/Test_Main1_Square.java
@@ -0,0 +1,25 @@
+package com.lean.project.euler;
+
+import org.testng.annotations.Test;
+
+import com.lean.project.euler.euler0006.Main1;
+
+import junit.framework.Assert;
+
+public class Test_Main1_Square {
+	@Test
+	public void testSquareSum0() {
+		long num = 0;
+		long ex = 0;
+		long r = Main1.squareSum(num);
+		Assert.assertEquals(ex, r);
+	}
+
+	@Test
+	public void testSquareSum10() {
+		long num = 10;
+		long ex = 385;
+		long r = Main1.squareSum(num);
+		Assert.assertEquals(ex, r);
+	}
+}
