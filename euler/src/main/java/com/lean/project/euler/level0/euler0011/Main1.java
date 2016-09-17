bc12de7e598be3db3de3375d507f748490090a68
zhangshuai <tozhangshuai@qq.com>
2016-09-17 10:14:26 +0800

    增加由上至下的计算方法

---
+++ euler/src/main/java/com/lean/project/euler/level0/euler0011/Main1.java
@@ -0,0 +1,44 @@
+package com.lean.project.euler.level0.euler0011;
+
+public class Main1 {
+	public static void main(String[] args) {
+	}
+
+	/**
+	 * 安装从上到下、从下到上、从左到右、从右到左、从左上到右下、从右上到左下的顺序遍历返回step个数字组成的字符串<br>
+	 * 如果遍历不到，返回null
+	 * 
+	 * @param matrix
+	 * @param step
+	 * @param count
+	 * @return
+	 */
+	public static String ergodicMatrix(int[][] matrix, int step, int count) {
+		if (count == 0) {
+			return null;
+		}
+		if (step > matrix.length && step > matrix[0].length) {
+			return null;
+		}
+		int exCount = 0;
+		String exString = "";
+		// 从上到下
+		for (int j = 0; j < matrix[0].length; j++) {
+			exString = "";
+			int countBack = 0;
+			for (int i = countBack++; i < matrix.length; i++) {
+				exString += matrix[i][j];
+				if (exString.length() == step) {
+					i = countBack++;
+					i--;
+					if (++exCount == count) {
+						return exString;
+					}
+					exString = "";
+				}
+			}
+		}
+		return null;
+
+	}
+}
---
+++ euler/src/test/java/com/lean/project/euler/level0/euler0011/Test_eraodicMatrix.java
@@ -0,0 +1,42 @@
+package com.lean.project.euler.level0.euler0011;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test_eraodicMatrix {
+	/**
+	 * 测试返回值为null的情况
+	 */
+	@Test
+	public void test_eraodicMatrix0() {
+		int[][] testMatrix = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		String r1 = Main1.ergodicMatrix(testMatrix, 4, 2);
+		Assert.assertEquals(r1, null, "r1 : ");
+
+		// String r2 = Main1.ergodicMatrix(testMatrix, 3, 3);
+		// Assert.assertEquals(r2, null, "r2 : ");
+
+	}
+
+	/**
+	 * 测试“由上到下”
+	 */
+	@Test
+	public void test_eraodicMatrix1() {
+		int[][] testMatrix = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		String r1 = Main1.ergodicMatrix(testMatrix, 2, 1);
+		Assert.assertEquals(r1, "13", "r1 : ");
+
+		String r2 = Main1.ergodicMatrix(testMatrix, 2, 2);
+		Assert.assertEquals(r2, "35", "r2 : ");
+
+		String r3 = Main1.ergodicMatrix(testMatrix, 2, 3);
+		Assert.assertEquals(r3, "24", "r3 : ");
+
+		String r4 = Main1.ergodicMatrix(testMatrix, 2, 4);
+		Assert.assertEquals(r4, "46", "r4 : ");
+
+	}
+
+	
+}
