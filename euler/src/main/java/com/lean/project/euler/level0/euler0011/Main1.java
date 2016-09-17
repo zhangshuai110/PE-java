22dedaa986bc92d5d16f48ffd966439fc0e369fd
zhangshuai <tozhangshuai@qq.com>
2016-09-17 13:17:13 +0800

    修改自上而下的bug

--- euler/src/main/java/com/lean/project/euler/level0/euler0011/Main1.java
+++ euler/src/main/java/com/lean/project/euler/level0/euler0011/Main1.java
@@ -21,59 +21,91 @@ public class Main1 {
 			return null;
 		}
 		int exCount = 0;
+		// 期望得到的每个小string
 		String exString = "";
+		// 每个exString的步长
+		int exStringStep = 0;
+		// System.out.println("matrix.length:" + matrix.length);
+		// System.out.println("matrix[0].length:" + matrix[0].length);
 		// 从上到下
 		for (int j = 0; j < matrix[0].length; j++) {
 			exString = "";
+			exStringStep = 0;
 			int countBack = 0;
-			for (int i = countBack++; i < matrix.length; i++) {
+			for (int i = countBack; i < matrix.length; i++) {
+//				System.out.println(j);
 				exString += matrix[i][j];
-				if (exString.length() == step) {
+				exString += ",";
+				exStringStep++;
+				if (exStringStep == step) {
 					i = countBack++;
-					i--;
-					if (++exCount == count) {
+					exCount++;
+					// 等于预期的个数，返回
+					if (exCount == count) {
 						return exString;
 					}
+					// 重新再来
 					exString = "";
+					exStringStep = 0;
 				}
 			}
+			// System.out.println(" ");
 		}
 
 		// 从下到上
-		for (int j = 0; j < matrix[0].length; j++) {
-			exString = "";
-			int countBack = matrix.length - 1;
-			for (int i = countBack; i >= 0; i--) {
-				exString += matrix[i][j];
-				if (exString.length() == step) {
-					i = --countBack;
-					i++;
-
-					if (++exCount == count) {
-						return exString;
-					}
-					exString = "";
-				}
-			}
-		}
+		// for (int j = 0; j < matrix.length; j++) {
+		// exString = "";
+		// int countBack = matrix[0].length - 1;
+		// for (int i = countBack; i >= 0; i--) {
+		// exString += matrix[i][j];
+		// if (exString.length() == step) {
+		// i = --countBack;
+		// i++;
+		//
+		// if (++exCount == count) {
+		// return exString;
+		// }
+		// exString = "";
+		// }
+		// }
+		// }
 
 		// 从左到右
-		for (int i = 0; i < matrix[0].length; i++) {
-			exString = "";
-			int countBack = 0;
-			for (int j = countBack; j < matrix.length; j++) {
-				exString += matrix[i][j];
-				if (exString.length() == step) {
-					j = ++countBack;
-					j--;
+		// for (int i = 0; i < matrix[0].length; i++) {
+		// exString = "";
+		// int countBack = 0;
+		// for (int j = countBack; j < matrix.length; j++) {
+		// exString += matrix[i][j];
+		// if (exString.length() == step) {
+		// j = ++countBack;
+		// j--;
+		//
+		// if (++exCount == count) {
+		// return exString;
+		// }
+		// exString = "";
+		// }
+		// }
+		// }
 
-					if (++exCount == count) {
-						return exString;
-					}
-					exString = "";
-				}
-			}
-		}
+		// // 从右到左
+		// for (int i = 0; i < matrix[0].length; i++) {
+		// exString = "";
+		// int countBack = matrix.length - 1;
+		// for (int j = countBack; j < matrix.length; j++) {
+		// exString += matrix[i][j];
+		// if (exString.length() == step) {
+		// j = ++countBack;
+		// j--;
+		//
+		// if (++exCount == count) {
+		// return exString;
+		// }
+		// exString = "";
+		// }
+		// }
+		// }
+
 		return null;
 
 	}
--- euler/src/test/java/com/lean/project/euler/level0/euler0011/Test_eraodicMatrix.java
+++ euler/src/test/java/com/lean/project/euler/level0/euler0011/Test_eraodicMatrix.java
@@ -23,64 +23,125 @@ public class Test_eraodicMatrix {
 	 */
 	@Test
 	public void test_eraodicMatrix1() {
-		int[][] testMatrix = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		int[][] testMatrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 }, { 13, 14, 15 }, { 16, 17, 18 } };
 		String r1 = Main1.ergodicMatrix(testMatrix, 2, 1);
-		Assert.assertEquals(r1, "13", "1r1 : ");
+		Assert.assertEquals(r1, "1,4,", "4r1 : ");
 
 		String r2 = Main1.ergodicMatrix(testMatrix, 2, 2);
-		Assert.assertEquals(r2, "35", "1r2 : ");
+		Assert.assertEquals(r2, "4,7,", "4r2 : ");
 
 		String r3 = Main1.ergodicMatrix(testMatrix, 2, 3);
-		Assert.assertEquals(r3, "24", "1r3 : ");
+		Assert.assertEquals(r3, "7,10,", "4r3 : ");
 
-		String r4 = Main1.ergodicMatrix(testMatrix, 2, 4);
-		Assert.assertEquals(r4, "46", "1r4 : ");
+		String r4 = Main1.ergodicMatrix(testMatrix, 2, 5);
+		Assert.assertEquals(r4, "13,16,", "4r4 : ");
 
+		String r5 = Main1.ergodicMatrix(testMatrix, 2, 6);
+		Assert.assertEquals(r5, "2,5,", "4r5 : ");
+
+		String r6 = Main1.ergodicMatrix(testMatrix, 2, 10);
+		Assert.assertEquals(r6, "14,17,", "4r6 : ");
+
+		String r7 = Main1.ergodicMatrix(testMatrix, 2, 12);
+		Assert.assertEquals(r7, "6,9,", "4r7 : ");
+
+		String r8 = Main1.ergodicMatrix(testMatrix, 2, 15);
+		Assert.assertEquals(r8, "15,18,", "4r8 : ");
 	}
 
 	/**
 	 * 测试“由下到上”
 	 */
-	@Test
-	public void test_eraodicMatrix2() {
-		int[][] testMatrix = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-		String r1 = Main1.ergodicMatrix(testMatrix, 2, 5);
-		Assert.assertEquals(r1, "53", "2r1 : ");
-
-		String r2 = Main1.ergodicMatrix(testMatrix, 2, 6);
-		Assert.assertEquals(r2, "31", "2r2 : ");
-
-		String r3 = Main1.ergodicMatrix(testMatrix, 2, 7);
-		Assert.assertEquals(r3, "64", "2r3 : ");
-
-		String r4 = Main1.ergodicMatrix(testMatrix, 2, 8);
-		Assert.assertEquals(r4, "42", "2r4 : ");
-
-	}
+	// @Test
+	// public void test_eraodicMatrix2() {
+	// int[][] testMatrix = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+	// String r1 = Main1.ergodicMatrix(testMatrix, 2, 5);
+	// Assert.assertEquals(r1, "53", "2r1 : ");
+	//
+	// String r2 = Main1.ergodicMatrix(testMatrix, 2, 6);
+	// Assert.assertEquals(r2, "31", "2r2 : ");
+	//
+	// String r3 = Main1.ergodicMatrix(testMatrix, 2, 7);
+	// Assert.assertEquals(r3, "64", "2r3 : ");
+	//
+	// String r4 = Main1.ergodicMatrix(testMatrix, 2, 8);
+	// Assert.assertEquals(r4, "42", "2r4 : ");
+	//
+	// }
 
 	/**
 	 * 测试“由左到右”
 	 */
-	@Test
-	public void test_eraodicMatrix3() {
-		int[][] testMatrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-		String r1 = Main1.ergodicMatrix(testMatrix, 2, 13);
-		Assert.assertEquals(r1, "12", "3r1 : ");
+	// @Test
+	// public void test_eraodicMatrix3() {
+	// int[][] testMatrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	// String r1 = Main1.ergodicMatrix(testMatrix, 2, 13);
+	// Assert.assertEquals(r1, "12", "3r1 : ");
+	//
+	// String r2 = Main1.ergodicMatrix(testMatrix, 2, 14);
+	// Assert.assertEquals(r2, "23", "3r2 : ");
+	//
+	// String r3 = Main1.ergodicMatrix(testMatrix, 2, 15);
+	// Assert.assertEquals(r3, "45", "3r3 : ");
+	//
+	// String r4 = Main1.ergodicMatrix(testMatrix, 2, 16);
+	// Assert.assertEquals(r4, "56", "3r4 : ");
+	//
+	// String r5 = Main1.ergodicMatrix(testMatrix, 2, 17);
+	// Assert.assertEquals(r5, "78", "3r5 : ");
+	//
+	// String r6 = Main1.ergodicMatrix(testMatrix, 2, 18);
+	// Assert.assertEquals(r6, "89", "3r6 : ");
+	// }
 
-		String r2 = Main1.ergodicMatrix(testMatrix, 2, 14);
-		Assert.assertEquals(r2, "23", "3r2 : ");
+	/**
+	 * 测试“由左到右”
+	 */
+	// @Test
+	// public void test_eraodicMatrix4() {
+	// int[][] testMatrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11,
+	// 12 }, { 13, 14, 15 }, { 16, 17, 18 } };
+	// String r1 = Main1.ergodicMatrix(testMatrix, 2, 1);
+	// Assert.assertEquals(r1, "14", "4r1 : ");
+	//
+	// String r2 = Main1.ergodicMatrix(testMatrix, 2, 2);
+	// Assert.assertEquals(r2, "47", "4r2 : ");
+	//
+	// String r3 = Main1.ergodicMatrix(testMatrix, 2, 3);
+	// Assert.assertEquals(r3, "1", "4r3 : ");
 
-		String r3 = Main1.ergodicMatrix(testMatrix, 2, 15);
-		Assert.assertEquals(r3, "45", "3r3 : ");
+	// String r4 = Main1.ergodicMatrix(testMatrix, 2, 16);
+	// Assert.assertEquals(r4, "56", "4r4 : ");
+	//
+	// String r5 = Main1.ergodicMatrix(testMatrix, 2, 17);
+	// Assert.assertEquals(r5, "78", "4r5 : ");
+	//
+	// String r6 = Main1.ergodicMatrix(testMatrix, 2, 18);
+	// Assert.assertEquals(r6, "89", "4r6 : ");
+	// }
 
-		String r4 = Main1.ergodicMatrix(testMatrix, 2, 16);
-		Assert.assertEquals(r4, "56", "3r4 : ");
-
-		String r5 = Main1.ergodicMatrix(testMatrix, 2, 17);
-		Assert.assertEquals(r5, "78", "3r5 : ");
-
-		String r6 = Main1.ergodicMatrix(testMatrix, 2, 18);
-		Assert.assertEquals(r6, "89", "3r6 : ");
-	}
-
+	/**
+	 * 测试“由右到左”
+	 */
+	// @Test
+	// public void test_eraodicMatrix5() {
+	// int[][] testMatrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	// String r1 = Main1.ergodicMatrix(testMatrix, 2, 19);
+	// Assert.assertEquals(r1, "32", "4r1 : ");
+	//
+	// String r2 = Main1.ergodicMatrix(testMatrix, 2, 20);
+	// Assert.assertEquals(r2, "21", "4r2 : ");
+	//
+	// String r3 = Main1.ergodicMatrix(testMatrix, 2, 21);
+	// Assert.assertEquals(r3, "65", "4r3 : ");
+	//
+	// String r4 = Main1.ergodicMatrix(testMatrix, 2, 22);
+	// Assert.assertEquals(r4, "54", "4r4 : ");
+	//
+	// String r5 = Main1.ergodicMatrix(testMatrix, 2, 23);
+	// Assert.assertEquals(r5, "98", "4r5 : ");
+	//
+	// String r6 = Main1.ergodicMatrix(testMatrix, 2, 24);
+	// Assert.assertEquals(r6, "87", "4r6 : ");
+	// }
 }
